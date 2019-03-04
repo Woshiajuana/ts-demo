96f5218d25d05dbb1d7d17c4fdbe6af16437bbdc
Woshiajuana <zhigang.chen@owulia.com>
2019-03-04 13:29:46 +0800

    提交

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
 
-module.exports = {
+import ArrayLib                 from './lib/array.lib'
+import DateLib                  from './lib/date.lib'
+import InputLib                 from './lib/input.lib'
+import ExtractLib               from './lib/extract.lib'
+import VerifyLib                from './lib/verify.lib'
+import MoneyLib                 from './lib/money.lib'
+import TypeLib                  from './lib/type.lib'
+import RegularLib               from './lib/regular.lib'
 
-    /**
-     * 查询数组序列
-     * @param  arr      [Array]                         需要查询的数组
-     * @param  filter   [Function, String, Number]      过滤条件
-     * */
-    findFirstIndexForArr (arr = [], filter = () => {}) {
-        if (!this.isArray(arr)) throw Error('arr must is array');
-        let index = -1;
-        arr.forEach((item, i) => {
-            if(typeof filter === 'function' && filter(item)) return index = i;
-            if(typeof filter !== 'function' && item === filter) return index = i;
-        });
-        return index;
-    },
-
-    /**
-     * 查询数组序列
-     * @param  arr      [Array]                         需要查询的数组
-     * @param  filter   [Function, String, Number]      过滤条件
-     * */
-    findLastIndexForArr (arr = [], filter = () => {}) {
-        if (!this.isArray(arr)) throw Error('arr must is array');
-        arr = arr.reverse();
-        return this.findFirstIndexForArr(arr, filter);
-    },
-
-    /**
-     * 判断是否是数组的方法
-     * @param  obj      [Array]         需要查询的对象
-     * */
-    isArray(obj = '') {
-        return Object.prototype.toString.call(obj) === '[object Array]';
-    }
-
-};
+export default {
+    ArrayLib,
+    DateLib,
+    InputLib,
+    ExtractLib,
+    VerifyLib,
+    MoneyLib,
+    TypeLib,
+    RegularLib,
+}
