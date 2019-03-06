6fb4a17ff8022497ff2a89f92b9ba5e60e2e71c6
Woshiajuana <zhigang.chen@owulia.com>
2019-03-06 11:01:11 +0800

    提交

--- test/clone.test.js
+++ test/clone.test.js
@@ -10,6 +10,8 @@ import {
 let x = [1,2,3];
 let y = [2,3,4];
 
+log(`测试 parseStringify ==========================`);
 log(`拷贝x：`, parseStringify(x));
 log(`比较x与拷贝x的值是否一致：`, parseStringify(x) === x);
 
+log(`测试 extend ==========================`);
