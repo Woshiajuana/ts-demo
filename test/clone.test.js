09e7748e49c75c179c25ec62003d6e22617712ec
Woshiajuana <zhigang.chen@owulia.com>
2019-03-08 14:58:21 +0800

    提交

--- test/clone.test.js
+++ test/clone.test.js
@@ -18,3 +18,34 @@ log(`测试 extend ==========================`);
 log(extend(true, [1,2,3], [4,5,6]));
 log(extend(true, [1,2,3], [4,5,6], {x:1}));
 log(extend(true, [1,2,3], [4,5,6], {x:1}));
+
+var obj1 = {
+    a: 1,
+    b: { b1: 1, b2: 2 }
+};
+
+var obj2 = {
+    b: { b1: 3, b3: 4 },
+    c: 3
+};
+
+var obj3 = {
+    d: 4
+}
+
+log(extend(true, obj1, obj2, obj3));
+
+var obj1 = {
+    a: 1,
+    b: {
+        c: 2
+    }
+}
+
+var obj2 = {
+    b: {
+        c: [5],
+
+    }
+}
+log(extend(true, obj1, obj2));
