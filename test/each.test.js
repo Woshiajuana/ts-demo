f4a0131ddf4ed79a3e4485fb0f19d4d242530dd4
Woshiajuana <zhigang.chen@owulia.com>
2019-03-13 17:57:28 +0800

    tijiao

--- test/each.test.js
+++ test/each.test.js
@@ -1,14 +1,12 @@
 
-import {log}                from 'wow-cmd'
+import log                  from './../uitl/log.util'
 import each                 from './../lib/each.lib'
-
-let arr = ['one', 'two', 'three', 'four'];
-each(arr, (item, index) => {
-    log(item);
-    if (item === 'two') {
-        arr.shift();
-    }
-});
+// each(arr, (item, index) => {
+//     log(item);
+//     if (item === 'two') {
+//         arr.shift();
+//     }
+// });
 
 // arr.forEach( (item, index) => {
 //     log(item);
@@ -16,3 +14,25 @@ each(arr, (item, index) => {
 //         arr.shift();
 //     }
 // });
+
+
+let per = ['one', 'two', 'three', 'four'];
+let p = per.map((item) => {
+    return item;
+});
+log('p', p);
+
+let arr = ['one', 'two', 'three', 'four'];
+let a = arr.map((item) => {
+    return item;
+});
+log('a', a);
+
+var person = ['xxxx', 'xxxxxxxx']
+var name = person.map((item) => {
+    return item;
+})
+var name1 = person.map((item) => {
+    return item;
+})
+log('name', name);
