6002034da9e5e56ba6af69b196f5251537f7ce31
Woshiajuana <zhigang.chen@owulia.com>
2021-06-12 20:53:03 +0800

    subcode

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -8,22 +8,20 @@ interface IStorageData<T> {
 }
 
 interface IStorage {
-    setItem<T>(key: string, value: T, expire: number): T,
+    setItem<T>(key: string, value: T, expire?: number): T,
     getItem<T>(key: string, defValue: T | null): T | null,
     removeItem(key: string): void,
     updateItem<T extends [] | object>(key: string, value: T, expire?: number): T,
     clear(key: string): void,
 }
 
-const setItem = function<T> (fn: typeFn, key: string, value: T, expire: number) : T {
-    const storage = window[fn];
-    storage.setItem(key, JSON.stringify({ key, value, expire }));
+const setItem = function<T> (fn: typeFn, key: string, value: T, expire: number = -1) : T {
+    window[fn].setItem(key, JSON.stringify({ key, value, expire }));
     return value;
 };
 
-const getItem = function<T>(fn: typeFn, key: string, defValue: T | null) {
-    const storage = window[fn];
-    let data = storage.getItem(key);
+const getItem = function<T> (fn: typeFn, key: string, defValue: T | null = null) : T | null {
+    const data = window[fn].getItem(key);
     if (data === null) {
         return defValue;
     }
@@ -35,25 +33,29 @@ const getItem = function<T>(fn: typeFn, key: string, defValue: T | null) {
     return defValue;
 };
 
-const removeItem = function (fn: typeFn, key: string) {
+const removeItem = function (fn: typeFn, key: string): void {
     window[fn].removeItem(key);
 };
 
 const updateItem = function<T extends [] | object> (fn: typeFn, key: string, value: T, expire?: number) : T {
-    let data = getItem<T>(fn, key, null);
-    if (data === null) {
-        return setItem<T>(fn, key, value, expire || -1);
+    const data = window[fn].getItem(key);
+    if (data) {
+        const { value: prevValue, expire: prevExpire } = JSON.parse(data) as IStorageData<T>;
+        if (prevExpire === -1 || Date.now() < prevExpire) {
+            if (Array.isArray(value)) {
+                value = [...prevValue as [], ...value] as T
+            } else {
+                value = Object.assign({}, prevValue, value);
+            }
+            if (!expire) {
+                expire = prevExpire;
+            }
+        }
     }
-    const { value: prevValue, expire: prevExpire } = data as IStorageData<T>;
-    if (Array.isArray(value)) {
-        value = [...prevValue as [], ...value] as T
-    } else {
-        value = Object.assign({}, prevValue, value);
-    }
-    return setItem<T>(fn, key, value, expire || prevExpire);
+    return setItem<T>(fn, key, value, expire);
 };
 
-const clear = function (fn: typeFn) {
+const clear = function (fn: typeFn) : void {
     window[fn].clear();
 };
 
