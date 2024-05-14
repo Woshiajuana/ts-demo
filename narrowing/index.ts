86860f117151b01959603048952b98f0d4126ae2
chenzhigang <979703986@qq.com>
2024-05-14 13:28:28 +0800

    chore: demo

--- narrowing/index.ts
+++ narrowing/index.ts
@@ -1,10 +1,10 @@
 type voidFunc = () => void;
-type X  = {
-  x: string
-}
+type X = {
+  x: string;
+};
 
 interface XX extends X {
-  xx: number
+  xx: number;
 }
 
 const f1: voidFunc = () => {
@@ -16,7 +16,6 @@ const f2: voidFunc = () => true;
 const f3: voidFunc = function () {
   return true;
 };
-
 
 function doSomething(): number {
   return 42;
@@ -30,7 +29,7 @@ function callMeMaybe(callback: () => void) {
 // expects void-returning function
 callMeMaybe(doSomething);
 
-export{}
+export {};
 
 interface Shape {}
 declare function getShape(): Shape;
@@ -49,3 +48,38 @@ function paintShape({ shape, xPos = 0, yPos = 0 }: PaintOptions) {
   //                             ^?
   // ...
 }
+
+// @errors: 2339
+function doStuff(values: ReadonlyArray<string>) {
+  // We can read from 'values'...
+  const copy = values.slice();
+  console.log(`The first value is ${values[0]}`);
+
+  // ...but we can't mutate 'values'.
+  // values.push("hello!");
+}
+
+type Arrayish = { [n: number]: unknown };
+type A = keyof Arrayish;
+//   ^?
+
+type Mapish = { [k: string]: unknown };
+
+type M = keyof Mapish;
+//   ^?
+
+type X1  = Uppercase<'x'>
+
+type Getters<Type> = {
+  [Property in keyof Type as `get${Capitalize<string & Property>}`]: () => Type[Property]
+};
+
+interface Person {
+  1: number
+  name: string;
+  age: number;
+  location: string;
+}
+
+type LazyPerson = Getters<Person>;
+//   ^?
